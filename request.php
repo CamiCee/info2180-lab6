182454a4e814ee180c40c4e0af4274e67dbed388
Camille Campbell <c.campbell1996@hotmail.com>
2016-11-12 06:14:51 +0000

    Exercise 4 changes

--- request.php
+++ request.php
@@ -1,20 +1,43 @@
 <?php
 
-// accept a term (keyword)
-// respond with a value
-
-
-$query = $_POST['q'];
-//$query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic bevserages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
-
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <dictionary>
+        <name>definition</name>
+        <txt>A statement of the exact meaning of a word, especially in a dictionary.</txt>
+        <author>-John Lowe</author>
+    </dictionary>
+    <dictionary>
+        <name>bar</name>
+        <txt>A place that sells alcholic beverages</txt>
+        <author>-John Culkin</author>
+    </dictionary>
+    <dictionary>
+        <name>ajax</name>
+        <txt>Technique which involves the use of javascript and xml (or JSON).</txt>
+        <author>-Charlie Lowe</author>
+    </dictionary>
+    <dictionary>
+        <name>html</name>
+        <txt>The standard markup language for creating web pages and web applications.</txt>
+        <author>-Charles Xavier</author>
+    </dictionary>
+    <dictionary>
+        <name>css</name>
+        <txt>A style sheet language used for describing the presentation of a document written in a markup language.</txt>
+        <author>-Britney Sanders</author>
+    </dictionary>
+    <dictionary>
+        <name>javascript</name>
+        <txt>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</txt>
+        <author>-Kim Smith</author>
+    </dictionary>
+    <dictionary>
+        <name>php</name>
+        <txt>A server-side scripting language, and a powerful tool for making dynamic and interactive websites.</txt>
+        <author>-Louis Carl</author>
+    </dictionary>
+</entries>';
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
