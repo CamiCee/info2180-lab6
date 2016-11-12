53567591d04db8afcc94b6e4e4f14cc6c3a97f70
Camille Campbell <c.campbell1996@hotmail.com>
2016-11-12 06:16:28 +0000

    Exercise 4 changes

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ window.onload = function()
 {
     
     var httpRequest;
+    var getXmlBtn = document.querySelector('#getAll');
     var defbutton= document.querySelector("#search");
     
-    defbutton.addEventListener("click",function(){
-         httpRequest = new XMLHttpRequest(); 
-         var word = document.querySelector('#word').value;
-         var url="request.php";
-         
+
+    defbutton.addEventListener('click', function() 
+    {
+         httpRequest = new XMLHttpRequest();
+        // GET Request
+        var url = "request.php";
         httpRequest.onreadystatechange = Req;
-        httpRequest.open('POST', url);
-        httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        httpRequest.send('q=' + encodeURIComponent(word));
-         
+        httpRequest.open('GET', url);
+        httpRequest.send();
     });
     
     function Req() 
@@ -22,15 +22,76 @@ window.onload = function()
         {
             if (httpRequest.status === 200) 
             {
-               
-                var response = httpRequest.responseText;
-                var result = document.querySelector('#result');
-                result.innerHTML = response;
-            } 
+                var word = document.querySelector('#word').value;
+                var temp=document.createTextNode(word);
+                var response = httpRequest.responseXML;
+                var results = document.querySelector('#result');
+                var dict = response.getElementsByTagName('dictionary');
+
+                var list = document.createElement('ol');
+                results.appendChild(list);
+                for (var x = 0; x < dict.length; x++)
+                {
+                    var words = document.createElement('ol');
+                    var text = document.createTextNode(dict[x].children[0].childNodes[0].nodeValue);
+                    if(dict[x].children[0].childNodes[0].nodeValue===word)
+                    {
+                        var text2 = document.createTextNode(dict[x].children[1].childNodes[0].nodeValue);
+                        var text3 = document.createTextNode(dict[x].children[2].childNodes[0].nodeValue);
+                        words.appendChild(text)
+                        words.appendChild(text2)
+                        words.appendChild(text3)
+                        list.appendChild(words);
+                    }
+                }
+            }
             else 
             {
-                alert('There was a problem with the request.');
+               alert('There was a problem with the request.');
             }
         }
     }
-}
+    
+    getXmlBtn.addEventListener('click', function() 
+    {
+         httpRequest = new XMLHttpRequest();
+
+        // GET Request
+        var url = "request.php";
+        httpRequest.onreadystatechange = getXml;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    });
+
+    function getXml() 
+    {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) 
+        {
+            if (httpRequest.status === 200) 
+            {
+                var response = httpRequest.responseXML;
+                var results = document.querySelector('#result');
+                var dict = response.getElementsByTagName('dictionary');
+
+                var list = document.createElement('ol');
+                results.appendChild(list);
+                for (var x = 0; x < dict.length; x++)
+                {
+                    var words = document.createElement('li');
+                    var text = document.createTextNode(dict[x].children[0].childNodes[0].nodeValue);
+                    var text2 = document.createTextNode(dict[x].children[1].childNodes[0].nodeValue);
+                    var text3 = document.createTextNode(dict[x].children[2].childNodes[0].nodeValue);
+                    words.appendChild(text)
+                    words.appendChild(text2)
+                    words.appendChild(text3)
+                    list.appendChild(words);
+                }
+            }
+            else 
+            {
+               alert('There was a problem with the request.');
+            }
+        }
+    }
+   
+};
